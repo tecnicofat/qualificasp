c60c99b62935a8a9e2ba011a0d503fcc5b123d74
tecnicofat <tecnico@estudar.life>
2025-07-11 13:12:48 -0300

    Add files via upload

---
+++ mobile/download content.bat
@@ -0,0 +1,64 @@
+mkdir "videos"
+mkdir "videos/horizontal"
+mkdir "videos/vertical"
+
+mkdir "fotos"
+mkdir "fotos/vertical"
+mkdir "fotos/horizontal"
+mkdir "fotos/square"
+
+curl.exe -L -o "videos/horizontal/01.mp4" "https://videos.pexels.com/video-files/4087103/4087103-uhd_2560_1440_25fps.mp4"
+curl.exe -L -o "videos/horizontal/02.mp4" "https://videos.pexels.com/video-files/3141320/3141320-uhd_2560_1440_25fps.mp4"
+curl.exe -L -o "videos/horizontal/03.mp4" "https://videos.pexels.com/video-files/7326212/7326212-hd_1920_1080_24fps.mp4"
+curl.exe -L -o "videos/horizontal/04.mp4" "https://videos.pexels.com/video-files/6337126/6337126-uhd_2560_1440_25fps.mp4"
+curl.exe -L -o "videos/horizontal/05.mp4" "https://videos.pexels.com/video-files/6963971/6963971-hd_1920_1080_25fps.mp4"
+curl.exe -L -o "videos/horizontal/06.mp4" "https://videos.pexels.com/video-files/4904253/4904253-uhd_2560_1440_24fps.mp4"
+curl.exe -L -o "videos/horizontal/07.mp4" "https://videos.pexels.com/video-files/8428489/8428489-uhd_2732_1440_25fps.mp4"
+curl.exe -L -o "videos/horizontal/08.mp4" "https://videos.pexels.com/video-files/5199842/5199842-uhd_2560_1440_25fps.mp4"
+curl.exe -L -o "videos/horizontal/09.mp4" "https://videos.pexels.com/video-files/8444033/8444033-uhd_2732_1440_25fps.mp4"
+curl.exe -L -o "videos/horizontal/10.mp4" "https://videos.pexels.com/video-files/3044154/3044154-uhd_2560_1440_25fps.mp4"
+
+curl.exe -L -o "videos/vertical/01.mp4" "https://videos.pexels.com/video-files/18744336/18744336-hd_1080_1920_24fps.mp4"
+curl.exe -L -o "videos/vertical/02.mp4" "https://videos.pexels.com/video-files/30850852/13193215_1440_2560_30fps.mp4"
+curl.exe -L -o "videos/vertical/03.mp4" "https://videos.pexels.com/video-files/27633462/12189355_1080_1920_25fps.mp4"
+curl.exe -L -o "videos/vertical/04.mp4" "https://videos.pexels.com/video-files/8104552/8104552-hd_1080_1774_30fps.mp4"
+curl.exe -L -o "videos/vertical/05.mp4" "https://videos.pexels.com/video-files/6466345/6466345-uhd_1440_2732_25fps.mp4"
+curl.exe -L -o "videos/vertical/06.mp4" "https://videos.pexels.com/video-files/8381147/8381147-uhd_1440_2560_25fps.mp4"
+curl.exe -L -o "videos/vertical/07.mp4" "https://videos.pexels.com/video-files/30108415/12913969_1080_1920_25fps.mp4"
+curl.exe -L -o "videos/vertical/08.mp4" "https://videos.pexels.com/video-files/7525494/7525494-hd_1080_1920_25fps.mp4"
+curl.exe -L -o "videos/vertical/09.mp4" "https://videos.pexels.com/video-files/19784279/19784279-uhd_1440_2560_60fps.mp4"
+curl.exe -L -o "videos/vertical/10.mp4" "https://videos.pexels.com/video-files/6202079/6202079-uhd_1440_2560_24fps.mp4"
+
+curl.exe -L -o "fotos/square/01.jpg" "https://images.pexels.com/photos/3892469/pexels-photo-3892469.jpeg
+curl.exe -L -o "fotos/square/02.jpg" "https://images.pexels.com/photos/3892468/pexels-photo-3892468.jpeg
+curl.exe -L -o "fotos/square/03.jpg" "https://images.pexels.com/photos/18205769/pexels-photo-18205769.jpeg
+curl.exe -L -o "fotos/square/04.jpg" "https://images.pexels.com/photos/8696529/pexels-photo-8696529.jpeg
+curl.exe -L -o "fotos/square/05.jpg" "https://images.pexels.com/photos/25819944/pexels-photo-25819944.jpeg
+curl.exe -L -o "fotos/square/06.jpg" "https://images.pexels.com/photos/15823267/pexels-photo-15823267.jpeg
+curl.exe -L -o "fotos/square/07.jpg" "https://images.pexels.com/photos/20212456/pexels-photo-20212456.jpeg
+curl.exe -L -o "fotos/square/08.jpg" "https://images.pexels.com/photos/4451666/pexels-photo-4451666.jpeg
+curl.exe -L -o "fotos/square/09.jpg" "https://images.pexels.com/photos/2074118/pexels-photo-2074118.jpeg
+curl.exe -L -o "fotos/square/10.jpg" "https://images.pexels.com/photos/10026524/pexels-photo-10026524.png
+
+curl.exe -L -o "fotos/horizontal/01.jpg" "https://images.pexels.com/photos/5749105/pexels-photo-5749105.jpeg"
+curl.exe -L -o "fotos/horizontal/02.jpg" "https://images.pexels.com/photos/32713518/pexels-photo-32713518.jpeg"
+curl.exe -L -o "fotos/horizontal/03.jpg" "https://images.pexels.com/photos/11079279/pexels-photo-11079279.jpeg"
+curl.exe -L -o "fotos/horizontal/04.jpg" "https://images.pexels.com/photos/5091141/pexels-photo-5091141.jpeg"
+curl.exe -L -o "fotos/horizontal/05.jpg" "https://images.pexels.com/photos/1546890/pexels-photo-1546890.jpeg"
+curl.exe -L -o "fotos/horizontal/06.jpg" "https://images.pexels.com/photos/1089570/pexels-photo-1089570.jpeg"
+curl.exe -L -o "fotos/horizontal/07.jpg" "https://images.pexels.com/photos/730191/pexels-photo-730191.jpeg"
+curl.exe -L -o "fotos/horizontal/08.jpg" "https://images.pexels.com/photos/8836426/pexels-photo-8836426.jpeg"
+curl.exe -L -o "fotos/horizontal/09.jpg" "https://images.pexels.com/photos/1619572/pexels-photo-1619572.jpeg"
+curl.exe -L -o "fotos/horizontal/10.jpg" "https://images.pexels.com/photos/6231588/pexels-photo-6231588.jpeg"
+ 
+curl.exe -L -o "fotos/vertical/01.jpg" "https://images.pexels.com/photos/8425210/pexels-photo-8425210.jpeg"
+curl.exe -L -o "fotos/vertical/02.jpg" "https://images.pexels.com/photos/8105039/pexels-photo-8105039.jpeg"
+curl.exe -L -o "fotos/vertical/03.jpg" "https://images.pexels.com/photos/4959984/pexels-photo-4959984.jpeg"
+curl.exe -L -o "fotos/vertical/04.jpg" "https://images.pexels.com/photos/28793164/pexels-photo-28793164.jpeg"
+curl.exe -L -o "fotos/vertical/05.jpg" "https://images.pexels.com/photos/11374693/pexels-photo-11374693.jpeg"
+curl.exe -L -o "fotos/vertical/06.jpg" "https://images.pexels.com/photos/8858950/pexels-photo-8858950.jpeg"
+curl.exe -L -o "fotos/vertical/07.jpg" "https://images.pexels.com/photos/14090902/pexels-photo-14090902.jpeg"
+curl.exe -L -o "fotos/vertical/08.jpg" "https://images.pexels.com/photos/11909323/pexels-photo-11909323.jpeg"
+curl.exe -L -o "fotos/vertical/09.jpg" "https://images.pexels.com/photos/29843736/pexels-photo-29843736.jpeg"
+curl.exe -L -o "fotos/vertical/10.jpg" "https://images.pexels.com/photos/11019175/pexels-photo-11019175.jpeg"
+
